099f898603d973f3fbcd817aa0313e9b07df0b66
DocWorks-Preview <docworkspreview@gmail.com>
2019-01-21 17:10:37 +0100

    DW CMS Message

--- PostProcessing/Runtime/Monitors/VectorscopeMonitor.cs
+++ PostProcessing/Runtime/Monitors/VectorscopeMonitor.cs
@@ -2,21 +2,21 @@ using System;
 
 namespace UnityEngine.Rendering.PostProcessing
 {
-    /// <summary>
-    /// This class holds settings for the Vectorscope monitor.
-    /// </summary>
-    [Serializable]
+    ///     <summary>
+        ///     This class holds settings for the Vectorscope monitor.
+        ///     </summary>
+            [Serializable]
     public sealed class VectorscopeMonitor : Monitor
     {
-        /// <summary>
-        /// The width and height of the rendered vectorscope.
-        /// </summary>
-        public int size = 256;
+        ///     <summary>
+                ///     The width and height of the rendered vectorscope.
+                ///     </summary>
+                        public int size = 256;
 
-        /// <summary>
-        /// The exposure multiplier applied to the vectorscope values.
-        /// </summary>
-        public float exposure = 0.12f;
+        ///     <summary>
+                ///     The exposure multiplier applied to the vectorscope values.
+                ///     </summary>
+                        public float exposure = 0.12f;
 
         ComputeBuffer m_Data;
         const int k_ThreadGroupSizeX = 16;
