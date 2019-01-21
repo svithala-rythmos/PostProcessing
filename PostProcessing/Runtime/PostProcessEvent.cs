c7b183af1e1ef4d53d2ab0df852afb8363c02b08
DocWorks-Preview <docworkspreview@gmail.com>
2019-01-21 17:10:42 +0100

    DW CMS Message

--- PostProcessing/Runtime/PostProcessEvent.cs
+++ PostProcessing/Runtime/PostProcessEvent.cs
@@ -2,27 +2,27 @@ using System.Collections.Generic;
 
 namespace UnityEngine.Rendering.PostProcessing
 {
-    /// <summary>
-    /// Injection points for custom effects.
-    /// </summary>
-    public enum PostProcessEvent
+    ///     <summary>
+        ///     Injection points for custom effects.
+        ///     </summary>
+            public enum PostProcessEvent
     {
-        /// <summary>
-        /// Effects at this injection points will execute before transparent objects are rendered.
-        /// </summary>
-        BeforeTransparent = 0,
+        ///     <summary>
+                ///     Effects at this injection points will execute before transparent objects are rendered.
+                ///     </summary>
+                        BeforeTransparent = 0,
 
-        /// <summary>
-        /// Effects at this injection points will execute after temporal anti-aliasing and before
-        /// builtin effects are rendered.
-        /// </summary>
-        BeforeStack = 1,
+        ///     <summary>
+                ///     Effects at this injection points will execute after temporal anti-aliasing and before
+                ///     builtin effects are rendered.
+                ///     </summary>
+                        BeforeStack = 1,
 
-        /// <summary>
-        /// Effects at this injection points will execute after builtin effects have been rendered
-        /// and before the final pass that does FXAA and applies dithering.
-        /// </summary>
-        AfterStack = 2,
+        ///     <summary>
+                ///     Effects at this injection points will execute after builtin effects have been rendered
+                ///     and before the final pass that does FXAA and applies dithering.
+                ///     </summary>
+                        AfterStack = 2,
     }
 
     // Box free comparer for our `PostProcessEvent` enum, else the runtime will box the type when
