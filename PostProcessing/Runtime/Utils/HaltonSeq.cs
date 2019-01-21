2947732d2f7fa770a860711434e5d369f59bc25f
DocWorks-Preview <docworkspreview@gmail.com>
2019-01-21 17:10:03 +0100

    DW CMS Message

--- PostProcessing/Runtime/Utils/HaltonSeq.cs
+++ PostProcessing/Runtime/Utils/HaltonSeq.cs
@@ -1,17 +1,17 @@
 namespace UnityEngine.Rendering.PostProcessing
 {
-    /// <summary>
-    /// Halton sequence utility.
-    /// </summary>
-    public static class HaltonSeq
+    ///     <summary>
+        ///     Halton sequence utility.
+        ///     </summary>
+            public static class HaltonSeq
     {
-        /// <summary>
-        /// Gets a value from the Halton sequence for a given index and radix.
-        /// </summary>
-        /// <param name="index">The sequence index</param>
-        /// <param name="radix">The sequence base</param>
-        /// <returns>A number from the Halton sequence between 0 and 1.</returns>
-        public static float Get(int index, int radix)
+        ///     <summary>
+                ///     Gets a value from the Halton sequence for a given index and radix.
+                ///     </summary>
+                ///     <param name="index">The sequence index</param>
+                ///     <param name="radix">The sequence base</param>
+                ///     <returns>A number from the Halton sequence between 0 and 1.</returns>
+                        public static float Get(int index, int radix)
         {
             float result = 0f;
             float fraction = 1f / (float)radix;
