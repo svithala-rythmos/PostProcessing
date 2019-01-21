b93d74949753e68c660dcadbe6ddb67e52f53749
DocWorks-Preview <docworkspreview@gmail.com>
2019-01-21 17:09:50 +0100

    DW CMS Message

--- PostProcessing/Editor/Utils/SerializedParameterOverride.cs
+++ PostProcessing/Editor/Utils/SerializedParameterOverride.cs
@@ -4,33 +4,33 @@ using UnityEngine.Rendering.PostProcessing;
 
 namespace UnityEditor.Rendering.PostProcessing
 {
-    /// <summary>
-    /// A wrapper used for <see cref="ParameterOverride{T}"/> serialization and easy access to the
-    /// underlying property and override state.
-    /// </summary>
-    public sealed class SerializedParameterOverride
+    ///     <summary>
+        ///     A wrapper used for <see cref="ParameterOverride{T}"/> serialization and easy access to the
+        ///     underlying property and override state.
+        ///     </summary>
+            public sealed class SerializedParameterOverride
     {
-        /// <summary>
-        /// The override state property of the serialized parameter.
-        /// </summary>
-        public SerializedProperty overrideState { get; private set; }
+        ///     <summary>
+                ///     The override state property of the serialized parameter.
+                ///     </summary>
+                        public SerializedProperty overrideState { get; private set; }
 
-        /// <summary>
-        /// The value property of the serialized parameter.
-        /// </summary>
-        public SerializedProperty value { get; private set; }
+        ///     <summary>
+                ///     The value property of the serialized parameter.
+                ///     </summary>
+                        public SerializedProperty value { get; private set; }
 
-        /// <summary>
-        /// An array of all attributes set on the original parameter.
-        /// </summary>
-        public Attribute[] attributes { get; private set; }
+        ///     <summary>
+                ///     An array of all attributes set on the original parameter.
+                ///     </summary>
+                        public Attribute[] attributes { get; private set; }
 
         internal SerializedProperty baseProperty;
 
-        /// <summary>
-        /// Returns the display name of the property.
-        /// </summary>
-        public string displayName
+        ///     <summary>
+                ///     Returns the display name of the property.
+                ///     </summary>
+                        public string displayName
         {
             get { return baseProperty.displayName; }
         }
@@ -48,12 +48,8 @@ namespace UnityEditor.Rendering.PostProcessing
             this.attributes = attributes;
         }
 
-        /// <summary>
-        /// Gets the attribute of type <c>T</c> from the original parameter.
-        /// </summary>
-        /// <typeparam name="T">The type of attribute to look for</typeparam>
-        /// <returns>And attribute or type <c>T</c>, or <c>null</c> if none has been found</returns>
-        public T GetAttribute<T>()
+        /// <!-- Badly formed XML comment ignored for member "M:UnityEditor.Rendering.PostProcessing.SerializedParameterOverride.GetAttribute``1" -->
+                        public T GetAttribute<T>()
             where T : Attribute
         {
             return (T)attributes.FirstOrDefault(x => x is T);
