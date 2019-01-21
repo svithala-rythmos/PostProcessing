b10dbc8af6174813234045c1670e3fc5286a821f
DocWorks-Preview <docworkspreview@gmail.com>
2019-01-21 17:10:15 +0100

    DW CMS Message

--- PostProcessing/Runtime/PostProcessEffectRenderer.cs
+++ PostProcessing/Runtime/PostProcessEffectRenderer.cs
@@ -1,59 +1,56 @@
 namespace UnityEngine.Rendering.PostProcessing
 {
-    /// <summary>
-    /// The base abstract class for all effect renderer types. If you're writing your own effect you
-    /// should rather use <see cref="PostProcessEffectRenderer{T}"/>.
-    /// </summary>
-    /// <seealso cref="PostProcessEffectRenderer{T}"/>
-    public abstract class PostProcessEffectRenderer
+    ///     <summary>
+        ///     The base abstract class for all effect renderer types. If you're writing your own effect you
+        ///     should rather use <see cref="PostProcessEffectRenderer{T}"/>.
+        ///     </summary>
+        ///     <seealso cref="T:UnityEngine.Rendering.PostProcessing.PostProcessEffectRenderer`1"/>
+            public abstract class PostProcessEffectRenderer
     {
-        /// <summary>
-        /// This member is set to <c>true</c> when <see cref="PostProcessLayer.ResetHistory"/> is
-        /// called by the user to reset temporal effects and other history-based effects.
-        /// </summary>
-        protected bool m_ResetHistory = true;
+        /// <!-- Badly formed XML comment ignored for member "F:UnityEngine.Rendering.PostProcessing.PostProcessEffectRenderer.m_ResetHistory" -->
+                        protected bool m_ResetHistory = true;
 
-        /// <summary>
-        /// Called when the renderer is created and its associated settings have been set.
-        /// </summary>
-        /// <seealso cref="PostProcessEffectRenderer{T}.settings"/>
-        public virtual void Init()
+        ///     <summary>
+                ///     Called when the renderer is created and its associated settings have been set.
+                ///     </summary>
+                ///     <seealso cref="P:UnityEngine.Rendering.PostProcessing.PostProcessEffectRenderer`1.settings"/>
+                        public virtual void Init()
         {
         }
 
-        /// <summary>
-        /// Override this method if your renderer needs access to any of the buffers defined in
-        /// <see cref="DepthTextureMode"/>.
-        /// </summary>
-        /// <returns>The currently set depth texture modes</returns>
-        /// <seealso cref="DepthTextureMode"/>
-        public virtual DepthTextureMode GetCameraFlags()
+        ///     <summary>
+                ///     Override this method if your renderer needs access to any of the buffers defined in
+                ///     <see cref="DepthTextureMode"/>.
+                ///     </summary>
+                ///     <returns>The currently set depth texture modes</returns>
+                ///     <seealso cref="!:DepthTextureMode"/>
+                        public virtual DepthTextureMode GetCameraFlags()
         {
             return DepthTextureMode.None;
         }
 
-        /// <summary>
-        /// Resets the history state for this renderer. This is automatically called when
-        /// <see cref="PostProcessLayer.ResetHistory"/> is called by the user.
-        /// </summary>
-        public virtual void ResetHistory()
+        ///     <summary>
+                ///     Resets the history state for this renderer. This is automatically called when
+                ///     <see cref="PostProcessLayer.ResetHistory"/> is called by the user.
+                ///     </summary>
+                        public virtual void ResetHistory()
         {
             m_ResetHistory = true;
         }
 
-        /// <summary>
-        /// Override this method to release any resource allocated by your renderer.
-        /// </summary>
-        public virtual void Release()
+        ///     <summary>
+                ///     Override this method to release any resource allocated by your renderer.
+                ///     </summary>
+                        public virtual void Release()
         {
             ResetHistory();
         }
 
-        /// <summary>
-        /// The render method called by <see cref="PostProcessLayer"/> when the effect is rendered.
-        /// </summary>
-        /// <param name="context">A context object</param>
-        public abstract void Render(PostProcessRenderContext context);
+        ///     <summary>
+                ///     The render method called by <see cref="PostProcessLayer"/> when the effect is rendered.
+                ///     </summary>
+                ///     <param name="context">A context object</param>
+                        public abstract void Render(PostProcessRenderContext context);
 
         internal abstract void SetSettings(PostProcessEffectSettings settings);
     }
