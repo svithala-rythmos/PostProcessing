b3ebbb94e28cd1385721643880b2ad59b1b35cb3
DocWorks-Preview <docworkspreview@gmail.com>
2019-01-21 17:10:36 +0100

    DW CMS Message

--- PostProcessing/Runtime/Attributes/TrackballAttribute.cs
+++ PostProcessing/Runtime/Attributes/TrackballAttribute.cs
@@ -8,31 +8,31 @@ namespace UnityEngine.Rendering.PostProcessing
             [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
     public sealed class TrackballAttribute : Attribute
     {
-        /// <summary>
-        /// Trackball modes. These are used to compute and display pre-filtered trackball vales in
-        /// the inspector.
-        /// </summary>
-        public enum Mode
+        ///     <summary>
+                ///     Trackball modes. These are used to compute and display pre-filtered trackball vales in
+                ///     the inspector.
+                ///     </summary>
+                        public enum Mode
         {
-            /// <summary>
-            /// Don't display pre-filtered values.
-            /// </summary>
-            None,
+            ///     <summary>
+                        ///     Don't display pre-filtered values.
+                        ///     </summary>
+                                    None,
 
-            /// <summary>
-            /// Display pre-filtered lift values.
-            /// </summary>
-            Lift,
+            ///     <summary>
+                        ///     Display pre-filtered lift values.
+                        ///     </summary>
+                                    Lift,
 
-            /// <summary>
-            /// Display pre-filtered gamma values.
-            /// </summary>
-            Gamma,
+            ///     <summary>
+                        ///     Display pre-filtered gamma values.
+                        ///     </summary>
+                                    Gamma,
 
-            /// <summary>
-            /// Display pre-filtered grain values.
-            /// </summary>
-            Gain
+            ///     <summary>
+                        ///     Display pre-filtered grain values.
+                        ///     </summary>
+                                    Gain
         }
 
         ///     <summary>
