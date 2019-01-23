bfd1f5a7bb110be69f6707babe2d5456e67aa3e9
CMSQATeam <cmsqa123@gmail.com>
2019-01-23 12:42:14 +0530

    DW CMS Message

--- PostProcessing/Runtime/PostProcessResources.cs
+++ PostProcessing/Runtime/PostProcessResources.cs
@@ -173,12 +173,15 @@ namespace UnityEngine.Rendering.PostProcessing
 
         
         
+        
         [Serializable]
         public sealed class SMAALuts
         {
             
             
+            
             public Texture2D area;
+            
             
             
             public Texture2D search;
